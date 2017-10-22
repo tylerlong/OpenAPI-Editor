bd3e2f3b05cb773bcd13b82484f216f1b7396d12
Tyler Long <tyler4long@gmail.com>
2017-10-22 20:06:55 +0800

    Use css to set case

--- src/web/components/App.js
+++ src/web/components/App.js
@@ -3,6 +3,7 @@ import { Input, Tabs } from 'antd'
 import { observer } from 'mobx-react'
 
 import FormItem from './FormItem'
+import Info from './Info'
 
 class App extends React.Component {
   render () {
@@ -10,30 +11,30 @@ class App extends React.Component {
     const openAPI = this.props.openAPI
     return (
       <div>
-        <FormItem label='OpenAPI Specification version'>
+        <FormItem label='OpenAPI specification version'>
           <Input value={openAPI.openapi} onChange={e => openAPI.update('openapi', e.target.value)} />
         </FormItem>
         <Tabs tabPosition='left' defaultActiveKey='info'>
-          <Tabs.TabPane tab='Info' key='info'>
-            Info
+          <Tabs.TabPane tab='info' key='info'>
+            <Info info={openAPI.info} />
           </Tabs.TabPane>
-          <Tabs.TabPane tab='Servers' key='servers'>
-            Servers
+          <Tabs.TabPane tab='servers' key='servers'>
+            servers
           </Tabs.TabPane>
-          <Tabs.TabPane tab='Paths' key='paths'>
-            Paths
+          <Tabs.TabPane tab='paths' key='paths'>
+            paths
           </Tabs.TabPane>
-          <Tabs.TabPane tab='Components' key='components'>
-            Components
+          <Tabs.TabPane tab='components' key='components'>
+            components
           </Tabs.TabPane>
-          <Tabs.TabPane tab='Security' key='security'>
-            Security
+          <Tabs.TabPane tab='security' key='security'>
+            security
           </Tabs.TabPane>
-          <Tabs.TabPane tab='Tags' key='tags'>
-            Tags
+          <Tabs.TabPane tab='tags' key='tags'>
+            tags
           </Tabs.TabPane>
           <Tabs.TabPane tab='externalDocs' key='externalDocs'>
-            ExternalDocs
+            externalDocs
           </Tabs.TabPane>
         </Tabs>
       </div>
